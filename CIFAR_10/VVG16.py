0774b9b20a57a1f2cd933b450dc2eb0fdacc6891
cuevas1208 <cuevas1208@gmail.com>
2018-02-20 22:17:15 -0800

    feature: starting vvg16 for CIFAR_10

--- CIFAR_10/VVG16.py
+++ CIFAR_10/VVG16.py
@@ -5,19 +5,31 @@ https://github.com/tensorflow/tensorflow/tree/master/tensorflow/contrib/slim
 
 import tensorflow as tf
 import tensorflow.contrib.slim.nets as nets
+import cifar10
+import os
 
+# The CIFAR-10 data-set is about 163 MB and will be downloaded automatically
+if os.path.exists("./data/CIFAR-10/"):
+    cifar10.data_path = "./data/CIFAR-10/"
+else:
+    cifar10.maybe_download_and_extract()
+class_names = cifar10.load_class_names()
+
+images_train, cls_train, labels_train = cifar10.load_training_data()
+images_test, cls_test, labels_test = cifar10.load_test_data()
+
+# init slim, get vgg net
 slim = tf.contrib.slim
 vgg = nets.vgg
 
-...
-
-train_log_dir = ...
+# create log dir
+train_log_dir = "./vgg_log"
 if not tf.gfile.Exists(train_log_dir):
   tf.gfile.MakeDirs(train_log_dir)
 
 with tf.Graph().as_default():
   # Set up the data loading:
-  images, labels = ...
+  images, labels = images_train.astype('float32'), cls_train
 
   # Define the model:
   predictions = vgg.vgg_16(images, is_training=True)
